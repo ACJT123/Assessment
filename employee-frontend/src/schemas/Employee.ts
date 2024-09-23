4119b9a3fa74ac0ec2f0bf7eae3940db0753d6c3
ACJT123 <acjtacjt@gmail.com>
2024-09-23 16:57:56 +0800

    fix(schema): add only spaces validation check for the form

--- employee-frontend/src/schemas/Employee.ts
+++ employee-frontend/src/schemas/Employee.ts
@@ -4,20 +4,40 @@ export const schema = yup.object().shape({
   name: yup
     .string()
     .required("Name is required")
-    .min(3, "Name must be at least 3 characters"),
+    .min(3, "Name must be at least 3 characters")
+    .test(
+      "is-not-only-spaces",
+      "Address cannot contain only spaces",
+      (value) => {
+        return value.trim().length > 0;
+      }
+    ),
+
   dept: yup.string().required("Department is required"),
+
   active: yup.boolean(),
+
   number: yup
     .number()
     .required("Employee number is required")
     .min(0, "Number must be positive"),
+
   email: yup
     .string()
     .email("Must be a valid email")
     .required("Email is required"),
+
   address: yup
     .string()
     .required("Address is required")
-    .min(10, "Address must be at least 10 characters"),
+    .min(10, "Address must be at least 10 characters")
+    .test(
+      "is-not-only-spaces",
+      "Address cannot contain only spaces",
+      (value) => {
+        return value.trim().length > 0;
+      }
+    ),
+
   photo: yup.mixed().required("Photo is required"),
 });
