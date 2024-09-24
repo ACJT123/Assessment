5f93bf9dd635cbefda1d515e3481f807c3341e8b
ACJT123 <acjtacjt@gmail.com>
2024-09-24 10:33:54 +0800

    break(employee-details): create an array to render the details

--- employee-frontend/src/components/employees/EmployeeDetails.tsx
+++ employee-frontend/src/components/employees/EmployeeDetails.tsx
@@ -13,18 +13,28 @@ export default function EmployeeDetails({
 
   const { name, photo, dept, number, active, email, address } = employees;
 
+  const details = [
+    { label: "Name", value: name },
+    { label: "Department", value: dept },
+    { label: "Number", value: number },
+    { label: "Active", value: active ? "Yes" : "No" },
+    { label: "Email", value: email },
+    { label: "Address", value: address },
+  ];
+
   return (
     <div className="p-4">
       <h1 className="text-center text-xl font-bold">Employee Details</h1>
-
-      <img src={photo} alt={name} className="mx-auto my-4 size-[150px] rounded-lg" />
-
-      <h2 className="mb-2 text-gray-700">Name: {name}</h2>
-      <p className="mb-2 text-gray-700">Department: {dept}</p>
-      <p className="mb-2 text-gray-700">Number: {number}</p>
-      <p className="mb-2 text-gray-700">Active: {active ? "Yes" : "No"}</p>
-      <p className="mb-2 text-gray-700">Email: {email}</p>
-      <p className="mb-2 text-gray-700">Address: {address}</p>
+      <img
+        src={photo}
+        alt={name}
+        className="mx-auto my-4 size-[150px] rounded-lg"
+      />
+      {details.map((detail, index) => (
+        <p key={index} className="mb-2 text-gray-700">
+          {detail.label}: {detail.value}
+        </p>
+      ))}
     </div>
   );
 }
